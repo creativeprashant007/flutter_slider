512e1868f06279cdd62e56922084c65b9c134c93
creativeprashant007 <prashanttimalsina18@gmail.com>
2024-05-31 10:20:57 +0100

    text changes

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -14,6 +14,7 @@ class _SecondPageState extends State<SecondPage> {
       child: Row(
         children: [
           Text("This is test TEst"),
+          Text("This is text text 2"),
         ],
       ),
     );
