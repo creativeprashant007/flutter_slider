fb3a5efdc5d83474bbccf93704342b08c46efd65
creativeprashant007 <prashanttimalsina18@gmail.com>
2024-07-28 12:19:59 +0100

    fix; text

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -31,6 +31,7 @@ class _SecondPageState extends State<SecondPage> {
           Text("This is text text 17"),
           Text("This is text text 18"),
           Text("This is text text 19"),
+          Text("This is text text 20"),
         ],
       ),
     );
